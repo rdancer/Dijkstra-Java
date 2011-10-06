9ee1e3913b5f9e1b0620512e460a3d12e675cfad
Jan Minář <rdancer@rdancer.org>
2011-10-06 17:34:05 +0100

    Create a graph data structure (adjacency matrix) from a String

---
+++ Graph.java
@@ -0,0 +1,102 @@
+/**
+ * Class Graph implementation.
+ * 
+ * Copyright 2011 Jan Minar <rdancer@rdancer.org>  All rights reserved.
+ *
+ * @author Jan Minar <rdancer@rdancer.org> 
+ */
+
+import java.nio.*;
+import java.io.*;
+import java.util.*;
+
+public class Graph
+{
+    protected final int vertexCount;
+    protected final int edgeCount;
+    
+    protected double[][] adjacencyMatrix;
+    
+    /**
+     * Constructor for objects of class AdjacencyMatrix
+     */
+    public Graph(String graphDescription)
+            throws Exception
+    {
+        Scanner sc = new Scanner(graphDescription);
+        sc.useDelimiter(",?[ \t\n]*");
+        vertexCount = sc.nextInt();
+        edgeCount = sc.nextInt();
+        
+        adjacencyMatrix = new double[vertexCount][edgeCount];
+        
+        // Adjacency matrix
+        
+        sc.useDelimiter("[)(, \t\n]*"); 
+                // Ignore commas and parentheses, even if they constitute syntax error
+        
+        try {
+            while (sc.hasNext())
+            {
+                int firstVertexIndex = sc.nextInt() - 1;
+                int secondVertexIndex = sc.nextInt() - 1;
+
+                //
+                // XXX Parsing the decimal number with Scanner does not work -- do it by hand
+                //
+                
+                //double thirdNumber = sc.nextDouble(); // not working
+                String s = "";
+                s += sc.next(); // the integral part
+                s += sc.next(); // the decimal point
+                s += sc.next(); // the decimal part
+                double edgeWeight = Double.valueOf(s).doubleValue();
+                        // XXX should use a decimal instead of a double to avoid rounding error
+                
+                adjacencyMatrix[firstVertexIndex][secondVertexIndex] = 
+                adjacencyMatrix[secondVertexIndex][firstVertexIndex] = edgeWeight;
+            }
+        }
+        catch (Exception e)
+        {   
+            throw e;
+        }
+    }
+  
+      
+    public String toString()
+    {
+        String s = "";
+        
+        
+        s += adjacencyMatrix().toString()
+                .replaceAll("\\[\\[", "[")
+                .replaceAll("]]", "]")
+                .replaceAll("], \\[", "]\n[");
+        s += "\n";
+
+        s += vertexCount + " vert" + (vertexCount == 1 ? "ex" : "ices");
+        s += ", ";
+        
+        s += edgeCount + " edge" + (edgeCount == 1 ? "" : "s");
+        
+        return s;
+    }
+    
+    protected List<List<Double>> adjacencyMatrix()
+    {
+        List<List<Double>> matrix = new ArrayList<List<Double>>();
+        
+        for (int vertex = 0; vertex < adjacencyMatrix.length; vertex++)
+        {
+            List<Double> singleRow = new ArrayList<Double>();
+            
+            for (int edge = 0; edge < adjacencyMatrix[vertex].length; edge++)
+                singleRow.add(adjacencyMatrix[vertex][edge]);
+            
+            matrix.add(singleRow);
+        }
+                
+        return matrix;
+    }
+}
