c2f01c0add4dd847cee8ec28625ee4749a2cbeee
Jan Minář <rdancer@rdancer.org>
2011-10-06 18:11:41 +0100

    Add a constructor that constructs the class from a file.  Update the README file; clean up the code.

--- Graph.java
+++ Graph.java
@@ -12,54 +12,65 @@ import java.util.*;
 
 public class Graph
 {
-    protected final int vertexCount;
-    protected final int edgeCount;
+    protected int vertexCount;
+    protected int edgeCount;
     
     protected double[][] adjacencyMatrix;
+
+    public Graph(File file)
+            throws Exception, IOException
+    {
+        String graphDescription = "";
+        Scanner sc = new Scanner(file);
+        
+        while (sc.hasNext())
+            graphDescription += sc.next();
+            
+       parseGraphDescription(graphDescription);
+    }
     
-    /**
-     * Constructor for objects of class AdjacencyMatrix
-     */
     public Graph(String graphDescription)
+            throws Exception
+    {
+        parseGraphDescription(graphDescription);
+    }
+
+    private void parseGraphDescription(String graphDescription)
             throws Exception
     {
         Scanner sc = new Scanner(graphDescription);
         sc.useDelimiter(",?[ \t\n]*");
         vertexCount = sc.nextInt();
         edgeCount = sc.nextInt();
-        
+    
         adjacencyMatrix = new double[vertexCount][edgeCount];
-        
-        // Adjacency matrix
-        
+                // if the dimensions are not sufficient, we're going to get an exception later
+
         sc.useDelimiter("[)(, \t\n]*"); 
                 // Ignore commas and parentheses, even if they constitute syntax error
         
-        try {
-            while (sc.hasNext())
-            {
-                int firstVertexIndex = sc.nextInt() - 1;
-                int secondVertexIndex = sc.nextInt() - 1;
 
-                //
-                // XXX Parsing the decimal number with Scanner does not work -- do it by hand
-                //
-                
-                //double thirdNumber = sc.nextDouble(); // not working
-                String s = "";
-                s += sc.next(); // the integral part
-                s += sc.next(); // the decimal point
-                s += sc.next(); // the decimal part
-                double edgeWeight = Double.valueOf(s).doubleValue();
-                        // XXX should use a decimal instead of a double to avoid rounding error
-                
-                adjacencyMatrix[firstVertexIndex][secondVertexIndex] = 
-                adjacencyMatrix[secondVertexIndex][firstVertexIndex] = edgeWeight;
-            }
-        }
-        catch (Exception e)
-        {   
-            throw e;
+       while (sc.hasNext())
+       {
+           // Some invalid input will result in an exception, some will be silently accepted
+           
+           int firstVertexIndex = sc.nextInt() - 1;
+           int secondVertexIndex = sc.nextInt() - 1;
+
+           //
+           // XXX Parsing the decimal number with Scanner does not work -- do it by hand
+           //
+            
+           //double thirdNumber = sc.nextDouble(); // not working
+           String s = "";
+           s += sc.next(); // the integral part
+           s += sc.next(); // the decimal point
+           s += sc.next(); // the decimal part
+           double edgeWeight = Double.valueOf(s).doubleValue();
+                    // XXX should use a decimal instead of a double to avoid rounding error
+
+           adjacencyMatrix[firstVertexIndex][secondVertexIndex] = 
+           adjacencyMatrix[secondVertexIndex][firstVertexIndex] = edgeWeight;
         }
     }
   
@@ -83,7 +94,7 @@ public class Graph
         return s;
     }
     
-    protected List<List<Double>> adjacencyMatrix()
+    public List<List<Double>> adjacencyMatrix()
     {
         List<List<Double>> matrix = new ArrayList<List<Double>>();
         
